38a07a4ae432a0d9b768222f87e397f7d063ef6b
artemykairyak <artemykairyak@yandex.com>
2019-08-19 17:19:06 +0400

    19.08.19

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -19,41 +19,33 @@ $(function(){
 
 	$('.header__calculate-btn, .works__calculate-btn').on('click', function(e) {
 		scrollTo('.calculating');
-    	e.preventDefault();
+		e.preventDefault();
+	});
+
+	$('.header .more-btn').on('click', function() {
+		scrollTo('.services');
+		e.preventDefault();
 	});
 
 	$('.services-item').on('click', function(e) {
 		scrollTo('.services');
-    	e.preventDefault();
+		e.preventDefault();
 	});
 
 	$('.advantages-item').on('click', function(e) {
 		scrollTo('.advantages');
-    	e.preventDefault();
+		e.preventDefault();
 	});
 
 	$('.deadlines-item').on('click', function(e) {
 		scrollTo('.deadlines');
-    	e.preventDefault();
+		e.preventDefault();
 	});
 
 	$('.contacts-item').on('click', function(e) {
 		scrollTo('.contacts');
-    	e.preventDefault();
+		e.preventDefault();
 	});
-
-	function scrollTo(destSelector) {
-		$padding = $('.header').css('padding-top');
-		$offset = $(destSelector).offset().top - parseInt($padding) - 15;
-		$('html').animate({ 
-            scrollTop: $offset
-        }, 1000);
-
-        if($('.mobile-header__burger').hasClass('active')) {
-			$('.mobile-header__burger').removeClass('active');
-			$('.mobile-menu').removeClass('active');
-        }
-	};
 
 	$('.works .table__item').on('mouseenter', function(e) {
 		if($(window).width() > 996) {
@@ -69,21 +61,21 @@ $(function(){
 		}
 	});
 
- 	$('.documents__nav .table__item img').on('click', function(e) {
- 		if($(window).width() < 680) {
- 			$('.modal__document img').attr('src', $(this).attr('src'));
- 			showModal($('.modal__document'));
- 		} else {
- 			$img = $(this).attr('src');
- 			$('.documents__main-slide .table__item img').fadeOut(200, function() {
- 				$(this).attr('src', $img);
- 			});
- 			$('.documents__main-slide .table__item img').fadeIn();
+	$('.documents__nav .table__item img').on('click', function(e) {
+		if($(window).width() < 680) {
+			$('.modal__document img').attr('src', $(this).attr('src'));
+			showModal($('.modal__document'));
+		} else {
+			$img = $(this).attr('src');
+			$('.documents__main-slide .table__item img').fadeOut(200, function() {
+				$(this).attr('src', $img);
+			});
+			$('.documents__main-slide .table__item img').fadeIn();
 
- 			$('.documents__main-slide .table__item .info__title').html($(this).parent().find('.info__title').html());
- 			$('.documents__main-slide .table__item .info__number').html($(this).parent().find('.info__number').html());
- 		} 		
- 	});
+			$('.documents__main-slide .table__item .info__title').html($(this).parent().find('.info__title').html());
+			$('.documents__main-slide .table__item .info__number').html($(this).parent().find('.info__number').html());
+		} 		
+	});
 
 	$('.objects__slider').owlCarousel({
 		navText: [$('.objects__slider-nav .slider-nav__prev'), $('.objects__slider-nav .slider-nav__next')],
@@ -139,24 +131,16 @@ $(function(){
 
 	$('.overlay').on('click', function() {
 		hideModal($('.modal'));
-	})
+	});
 
 	$('.modal__close').on('click', function() {
 		hideModal($(this).closest('.modal'));
 	});
 
-	function hideModal(modal) {
-		modal.animate({'opacity': '0'}, 200, function() {
-			modal.removeClass('active');
-			$('.overlay').hide();
-		});
-	}
-
-	function showModal(modal) {
-		$('.overlay').show();
-		modal.addClass('active');
-		modal.animate({'opacity': '1'}, 200);
-	}
+	$('.footer__item .policy, .modal__policy-link, .questions-form__policy').on('click', function(e) {
+		showModal($('.modal__policy'));
+		e.preventDefault();
+	})
 
 	$('.write-btn').on('click', function(e) {
 		showModal($('.modal__mail'));
@@ -191,72 +175,98 @@ $(function(){
 	})	
 
 	$('.location__close').on('click', function() {
-			$('.contacts').removeClass('tablet-active');
-			$('.contacts').removeClass('mobile-active');
+		$('.contacts').removeClass('tablet-active');
+		$('.contacts').removeClass('mobile-active');
 		$(this).parent().removeClass('active');
 	});
 
 	if($(window).width() > 996 && $(window).width() < 1355) {
 		$('.contacts').addClass('tablet-active');
-		} else if($(window).width() < 996) {
-			$('.contacts').addClass('mobile-active');
-		} else {
-			$('.contacts').removeClass('tablet-active');
-			$('.contacts').removeClass('mobile-active');
-		}
-
-	function onInitSlider () {
-		$('.documents__main-slide .table__item img').attr('src', $('.documents__nav .table__item:first img').attr('src'));
-		$('.documents__main-slide .info__title').html($('.documents__nav .table__item:first .info__title').html());
-		$('.documents__main-slide .info__number').html($('.documents__nav .table__item:first .info__number').html());
+		$('.contacts__location').removeClass('active');
+	} else if($(window).width() < 996) {
+		$('.contacts').addClass('mobile-active');
+		$('.contacts__location').removeClass('active');
+	} else {
+		$('.contacts').removeClass('tablet-active');
+		$('.contacts').removeClass('mobile-active');
 	}
 
-		ymaps.ready(function () {
-			var myMap = new ymaps.Map('map', {
-				center: [55.760851099950635, 37.75161043981168],
-				zoom: 16,
-				controls: []
+	ymaps.ready(function () {
+		var myMap = new ymaps.Map('map', {
+			center: [55.760851099950635, 37.75161043981168],
+			zoom: 16,
+			controls: []
 
-			}, {
-				searchControlProvider: 'yandex#search'
-			}),
+		}, {
+			searchControlProvider: 'yandex#search'
+		}),
 
-			MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div class="placemark_layout_container"><div class="circle_layout"></div></div>');
-        
+		MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div class="placemark_layout_container"><div class="circle_layout"></div></div>');
 
-        var myPlacemark = new ymaps.Placemark([55.760851099950635, 37.75161043981168], {
-        	  iconContent: '12'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-          iconLayout: MyIconContentLayout,
-            // Описываем фигуру активной области "Круг".
-            iconShape: {
-                type: 'Circle',
-                // Круг описывается в виде центра и радиуса
-                coordinates: [0, 0],
-                radius: 25
-            },
-            // Размеры метки.
-            iconImageSize: [68, 68],
-            iconImageOffset: [-12, 0]
-			});
 
-			myMap.geoObjects.add(myPlacemark);
-			myMap.behaviors.disable('scrollZoom');
-			myMap.behaviors.disable('drag');
+		var myPlacemark = new ymaps.Placemark([55.760851099950635, 37.75161043981168], {
+			iconContent: '12'
+		}, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: MyIconContentLayout,
+        // Описываем фигуру активной области "Круг".
+        iconShape: {
+        	type: 'Circle',
+            // Круг описывается в виде центра и радиуса
+            coordinates: [0, 0],
+            radius: 25
+        },
+        // Размеры метки.
+        iconImageSize: [68, 68],
+        iconImageOffset: [-12, 0]
+    });
 
-			myPlacemark.events.add('click', function () {
-				if($(window).width() > 996 && $(window).width() < 1355) {
-					$('.contacts').toggleClass('tablet-active');
-				} else if($(window).width() < 996) {
-					$('.contacts').toggleClass('mobile-active');
-				}
+		myMap.geoObjects.add(myPlacemark);
+		myMap.behaviors.disable('scrollZoom');
+		myMap.behaviors.disable('drag');
 
-				else {
-					$('.contacts__location').toggleClass('active');
-				}
-			 
-			});
+		myPlacemark.events.add('click', function () {
+			if($(window).width() > 996 && $(window).width() < 1355) {
+				$('.contacts').toggleClass('tablet-active');
+			} else if($(window).width() < 996) {
+				$('.contacts').toggleClass('mobile-active');
+			}
+			else {
+				$('.contacts__location').toggleClass('active');
+			}
+		});
 	});
 });
+
+function onInitSlider () {
+	$('.documents__main-slide .table__item img').attr('src', $('.documents__nav .table__item:first img').attr('src'));
+	$('.documents__main-slide .info__title').html($('.documents__nav .table__item:first .info__title').html());
+	$('.documents__main-slide .info__number').html($('.documents__nav .table__item:first .info__number').html());
+}
+
+function hideModal(modal) {
+	modal.animate({'opacity': '0'}, 200, function() {
+		modal.removeClass('active');
+		$('.overlay').hide();
+	});
+}
+
+function showModal(modal) {
+	$('.overlay').show();
+	modal.addClass('active');
+	modal.animate({'opacity': '1'}, 200);
+}
+
+function scrollTo(destSelector) {
+	$padding = $('.header').css('padding-top');
+	$offset = $(destSelector).offset().top - parseInt($padding) - 15;
+	$('html').animate({ 
+		scrollTop: $offset
+	}, 1000);
+
+	if($('.mobile-header__burger').hasClass('active')) {
+		$('.mobile-header__burger').removeClass('active');
+		$('.mobile-menu').removeClass('active');
+	}
+};
